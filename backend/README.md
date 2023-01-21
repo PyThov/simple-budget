c3e088fa8680ee972155e42c03f56ae7665889a1
PyThov <noahthovson@gmail.com>
2023-01-21 13:46:35 -0600

    updating file structure; adding poetry

---
+++ backend/README.md
@@ -0,0 +1,15 @@
+# Backend
+
+## Steps Taken
+ - Setup file structure
+   - /ui
+      - Init ReactJS project with Vite
+   - /backend
+      - Setup poetry
+      - /api
+      - /db
+ - Setup docker containers
+    - DB connection and running Python
+ - Setup database models
+    - Make sure this works with database initialization
+ - Setup API (*WIP*)
--- backend/db/simpleDB.py
+++ backend/db/simpleDB.py
@@ -1,15 +1,13 @@
 from sqlalchemy import create_engine
-import psycopg2
 
 def getEngine():
     return create_engine("postgresql://simpleuser:complexpassword@localhost/simpledb", echo=True, future=True)
 
-def main():
+def initializeDB():
     engine = getEngine()
     try:
-        from db_models import base, users, income, expenses, savings, recreational
+        from backend.models import base, users, income, expenses, savings, recreational
         base.Base.metadata.create_all(engine)
     except Exception as e:
         print(f"Error creating tables: {e}")
-
-main()
+    
